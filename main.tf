8ba0f5b2284861e3c1fae5ba7a5246a7ada2fb7d
Ophelia.Huang@ibm.com <Ophelia.Huang@ibm.com>
2022-01-14 12:07:35 +0000

    added ingress and egress security config

--- main.tf
+++ main.tf
@@ -59,3 +59,48 @@ resource "aws_route_table_association" "my-subnet-association" {
   subnet_id      = aws_subnet.dev-subnet.id
   route_table_id = aws_route_table.prod-route-table.id
 }
+
+# create a security group for internet access
+resource "aws_security_group" "allow_web_traffic" {
+  name        = "allow_web_traffic"
+  description = "Allow inbound and outbound traffic"
+  vpc_id      = aws_vpc.my-aws-vpc.id
+
+  ingress {
+    description      = "Allow HTTPS in-bound traffic from all ip addresses"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.my-aws-vpc.cidr_block]
+  }
+
+   ingress {
+    description      = "Allow HTTP in-bound traffic from all ip addresses"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.my-aws-vpc.cidr_block]
+  }
+
+   ingress {
+    description      = "Allow SSH in-bound traffic from all ip addresses"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.my-aws-vpc.cidr_block]
+  }
+
+
+  egress {
+    description      = "Allow outbound traffic from all portocols"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_web"
+  }
+}
